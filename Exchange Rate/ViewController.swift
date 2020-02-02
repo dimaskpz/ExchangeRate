6e1a631fec8c3fa425bb261173f2f43280a54be9
dimas kurnia <dimaskpz@gmail.com>
2020-02-02 14:38:30 +0700

    first push

---
+++ Exchange Rate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exchange Rate
+//
+//  Created by dimas pratama on 02/02/20.
+//  Copyright © 2020 dimas pratama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
